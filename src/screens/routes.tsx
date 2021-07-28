101ed01fbe4a0d9e8cf672223683899174dc3c8d
Sr-Lucas <lucastsantos.developer@gmail.com>
2021-07-28 20:50:59 -0300

    chore: changes directory name

--- src/screens/routes.tsx
+++ src/screens/routes.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Home from '../features/home';
+import Home from './home';
 
 const Routes: React.FC = () => {
   return (
