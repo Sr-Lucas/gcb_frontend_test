b97549f125dfba69889b22ccc253b560b6437c18
Sr-Lucas <lucastsantos.developer@gmail.com>
2021-07-30 17:09:19 -0300

    refactor: global style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,36 +5,75 @@ import 'react-toastify/dist/ReactToastify.css';
 import 'react-perfect-scrollbar/dist/css/styles.css';
 
 export default createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
+   html,
+   body, .root {
+     font-size: 16px;
+     min-height: 100vh;
 
-  html, body, #root {
-    max-height: 100vh;
-    max-width: 100vw;
+     padding: 0;
+     margin: 0;
+     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+   }
 
-    width: 100%;
-    height: 100%;
+   a {
+     color: inherit;
+     text-decoration: none;
+   }
 
-    @media (max-width: 600px) {
-      display: flex;
-    }
-  }
+   * {
+     box-sizing: border-box;
+     outline: none;
+     font-weight: 400;
+     font-family: 'Montserrat';
+   }
 
-  *, button, input {
-    border: 0;
-    background: none;
-    outline: transparent;
+   button {
+     border: none;
+     background: none;
+     font-family: 'Muli';
+     font-size: 16px;
+     font-weight: bold;
+     cursor: pointer;
+   }
+
+   span {
+    color: var(--text-color);
+   }
+
+   input {
+     border-style: none;
+   }
+
+   @font-face {
     font-family: 'Montserrat';
+    src: url('/Montserrat-Regular.ttf');
+    font-weight: 400;
+    font-style: normal;
   }
 
-  html {
-    background: var(--background-primary);
-  }
+  @font-face {
+     font-family: 'Montserrat';
+     src: url('/Montserrat-Bold.ttf');
+     font-weight: bold;
+     font-style: normal;
+   }
 
-  :root {
+   @font-face {
+     font-family: 'Muli';
+     src: url('/Muli.ttf');
+     font-weight: 400;
+     font-style: normal;
+   }
+
+   @font-face {
+     font-family: 'Muli';
+     src: url('/Muli-Bold.ttf');
+     font-weight: bold;
+     font-style: normal;
+   }
+
+    :root {
     --primary: #BADC58;
     --secondary: #FFFFFF;
     --white: #ffff;
